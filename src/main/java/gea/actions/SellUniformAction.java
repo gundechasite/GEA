11e8fbe3d448fb9bec4c61fea4195f9bfa8acab3
gundechasite <gundecha.site>
2017-04-08 10:51:48 +0530

    size numeric

--- src/main/java/gea/actions/SellUniformAction.java
+++ src/main/java/gea/actions/SellUniformAction.java
@@ -56,7 +56,7 @@ public class SellUniformAction extends GeaBasicAdAction {
 		      if (GeaUtility.isFieldEmpty(uniformSize[i])) {
 		    	  addActionError("The size must be entered");
 		      }
-		      if (GeaUtility.isFieldNumeric(uniformSize[i])) {
+		      if (!GeaUtility.isFieldNumeric(uniformSize[i])) {
 		    	  addActionError("The size entered should be a number");
 		      }
 		      if (GeaUtility.isFieldEmpty(uniformCondition[i])) {
